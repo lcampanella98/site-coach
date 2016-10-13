5562cfac8c37991ea5deb58cdd542a7fe697decb
lcampanella98 <enzocampanella98@gmail.com>
2016-10-13 12:07:45 -0400

    added support for the database configuration file

--- server.js
+++ server.js
@@ -8,22 +8,46 @@ var serve_static = require("serve-static");
 
 var app = connect();
 
+var SERVER_PORT = 8888;
+
+var dbConfigPath = "../../dbsetup/dbcosnfig.json";
+var dbConfigObj;
+
+if (fs.existsSync(dbConfigPath)) {
+    dbConfigObj = JSON.parse(fs.readFileSync(dbConfigPath));
+} else {
+    dbConfigObj = {
+        host: "localhost",
+        user: "root",
+        password: "password"
+    }
+}
+
 var sqlCon = mysql.createConnection({
-    host: "localhost",
-    user: "lcampanella",
-    password: "lorenzo98",
+    host: dbConfigObj["host"],
+    user: dbConfigObj["user"],
+    password: dbConfigObj["password"],
     database: "site_coach"
 });
 
 app.use(serve_static(path.join(__dirname, "public")));
-app.use("/getLogs", function(req, res, next) {
-    var q = "SELECT ROUND(AVG(load_time)) as avg_load_time, CAST(request_timestamp as DATE) as date FROM load_times GROUP BY date ORDER BY date";
-    sqlCon.query(q, function(fields, results, err) {
+app.use("/getLogs", function(req, res) {
+    var q = "SELECT ROUND(AVG(load_time)) as avg_load_time, CAST(request_timestamp as DATE) as date " +
+        "FROM load_times " +
+        "GROUP BY date " +
+        "ORDER BY date";
+    sqlCon.query(q, function(err, rows) {
+        if (err) {
+            res.writeHead(500);
+            res.write("A database error occurred:\n" + err.toString());
+            res.end();
+            return;
+        }
         res.writeHead(200, {"Content-Type": "application/json"});
-        res.write(JSON.stringify(results));
+        res.write(JSON.stringify(rows));
         res.end();
     });
 });
 
 var server = http.createServer(app);
-server.listen(8888);
+server.listen(SERVER_PORT);
