5686e1a041676770db0c7a34e35bb11c6013895f
lcampanella98 <enzocampanella98@gmail.com>
2016-10-18 00:51:33 -0400

    added more configuration options

--- server.js
+++ server.js
@@ -9,27 +9,30 @@ var serve_static = require("serve-static");
 
 var app = connect();
 
-var SERVER_PORT = 8888;
+var mysqlConOptions = {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "site_coach"
+};
+var serverPort = 8888;
 
-var dbConfigPath = path.join(__dirname, "dbconfig.json");
-var dbConfigObj;
+var configPath = path.join(__dirname, "config.json");
 
-if (fs.existsSync(dbConfigPath)) {
-    dbConfigObj = JSON.parse(fs.readFileSync(dbConfigPath));
-} else {
-    dbConfigObj = {
-        host: "localhost",
-        user: "root",
-        password: "password"
+if (fs.existsSync(configPath)) {
+    var configObj = JSON.parse(fs.readFileSync(configPath));
+    if (configObj["database"] !== undefined) {
+        dbConf = configObj["database"];
+        if (dbConf["host"] !== undefined) mysqlConOptions.host = dbConf["host"];
+        if (dbConf["port"] !== undefined) mysqlConOptions.port = dbConf["port"];
+        if (dbConf["user"] !== undefined) mysqlConOptions.user = dbConf["user"];
+        if (dbConf["password"] !== undefined) mysqlConOptions.password = dbConf["password"];
     }
+    if (configObj["serverPort"] !== undefined) serverPort = configObj["serverPort"];
 }
 
-var sqlCon = mysql.createConnection({
-    host: dbConfigObj["host"],
-    user: dbConfigObj["user"],
-    password: dbConfigObj["password"],
-    database: "site_coach"
-});
+var sqlCon = mysql.createConnection(mysqlConOptions);
 
 app.use(favicon(path.join(__dirname, "public/favicon-16x16.png")));
 app.use(serve_static(path.join(__dirname, "public")));
@@ -52,4 +55,4 @@ app.use("/getAllLogs", function (req, res) {
 });
 
 var server = http.createServer(app);
-server.listen(SERVER_PORT);
+server.listen(serverPort);
